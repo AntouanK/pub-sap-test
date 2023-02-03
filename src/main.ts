d061ad9169012d4990c9fda03bae11c510257d78
Antonis Karamitros <karamitros@duck.com>
2023-02-03 10:54:47 +0000

    add start logs with docs link

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,6 @@ app.use("/credit-card", routerCreditCard);
 addDocsRoute(app);
 
 app.listen(3333, () => {
-  console.log("app listening on port 3333!");
+  console.log("[start] app listening on port 3333!");
+  console.log("[start] Swagger docs available at http://localhost:3333/docs");
 });
